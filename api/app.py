57b8a992ef158564cebfb452b95db2f121e7a097
kalkidanzenebe <163387744+kalkidanzenebe@users.noreply.github.com>
2025-02-07 18:43:12 +0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -83,4 +83,4 @@ def home():
 # Run the API server
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=0000)
